1d04240732b032fd89c6a785abef93f02a584f8d
gitMilan <roenhorst.milan@gmail.com>
2022-06-06 16:47:31 +0200

    added some WIP assignment work

--- Assignment3/assignment3.sh
+++ Assignment3/assignment3.sh
@@ -1,1 +1,9 @@
-echo "test"
+ #!/bin/bash
+#SBATCH --time 2:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=16000
+module load Bowtie2
+export BOWTIE2_INDEXES=/data/p225083/BOWTIE2_INDEXES
+export DATA=/data/p225083
+bowtie2 -x human -U $DATA/all.fq -p 16 -S ${DATA}/output.sam
